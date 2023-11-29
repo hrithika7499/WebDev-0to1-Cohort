47bd7fe23286836eec9d8419639db403f079b7e1
Hrithika <rhrithikha04@gmail.com>
2023-11-29 21:31:43 +0530

    datatypes

---
+++ Datatypes/script.js
@@ -0,0 +1,43 @@
+//Question-1 :   Input the distance in Kilometer and Convert into Meter and Centimeter.
+ 
+/*
+const kilo=prompt("Enter distance in kilometers");
+ let meter;
+ let centimeter;
+ meter=kilo*1000;
+ console.log("DIstance in meters",meter);
+ centimeter=kilo*100000;
+ console.log("Distance in centimeter",centimeter);
+ */
+
+ //Question-2:    WAP to input radius of a circle and log the area of circle.
+
+ let radius=300;
+ let area=3.14*300*300;
+ console.log(area);
+
+ //Question-3:    WAP to input two numbers and perform arithmetic operations on those numbers.
+
+ let num1=prompt("Enter a number1");
+ let num2=prompt("Enter a number2");
+ let res;
+
+ function addNum(num1,num2){
+    res=num1+num2;
+    console.log(res);
+ }
+ function subNum(num1,num2){
+    res=num1-num2;
+    console.log(res);
+ }
+ function mulNum(num1,num2){
+    res=num1*num2;
+    console.log(res);
+ }
+ function divNum(num1,num2){
+    res=num1/num2;
+    console.log(res);
+ }
+
+ addNum(cd);
+ 
