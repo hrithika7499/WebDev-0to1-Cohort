d7dd418872e3d6e2f1cf0c3ccc212ad901600269
Hrithika <rhrithikha04@gmail.com>
2023-12-02 12:17:00 +0530

    controlstatements assignment

---
+++ ControlStatements/practise.js
@@ -0,0 +1,54 @@
+//WAP to check  time of day based on the current hour. For example, if it's before noon, log "Good morning.". (take time as an input).
+//taking hours to caluclating timer
+
+            // const date= new Date();
+            // const hours=date.getHours();
+
+            // if(hours<12){
+            //     console.log("Subha Hogayi Mamu!!!");
+            // }else if(hours>12 ){
+            //     console.log("Good Evening !! ")
+            // }
+
+ //WAP using Switch-case to display month name according to number, for eg: 1 => January. (take a number as input).
+
+            // let year=7;
+
+            // switch(year){
+            //     case 1:console.log("January!!")
+            //     break;
+            //     case 2:console.log("Feburary!!")
+            //     break;
+            //     case 3:console.log("March!!")
+            //     break;
+            //     case 4:console.log("April!!")
+            //     break;
+            //     case 5:console.log("May!!")
+            //     break;
+            //     case 6:console.log("June!!")
+            //     break;
+            //     case 7:console.log("July😍😍😎!!")
+            //     break;
+            //     case 8:console.log("August!!")
+            //     break;
+            //     default:console.log("MAYDAYYYYY!")
+            // }
+
+//wap a program to categorize a give temparature into high,low and medium ranges
+            // let temparature=20;
+            // if(temparature<=20){
+            //     console.log("Sardiyaa!!");
+            // }else if (temparature>20 && temparature<35){
+            //     console.log("Medium hai ji!!");
+            // }else if(temparature>=35){
+            //     console.log("High!!!")
+            // }
+
+// WAP to validate a username. If the username is less than 6 characters, log "Username too short"; if it's more than 15 characters, log "Username too long"; otherwise, log "Username accepted."
+
+const username="Mahendra Singh Dhoni";
+if(username.length <6){
+    console.log("Ahh too short name to win ICC trophies")
+}
+
+
